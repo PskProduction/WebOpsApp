86f89439eac9288f375631f55294f1e2c6cd93f9
Nikita Petrov <Nikita2871@yandex.ru>
2023-12-03 17:35:03 +0300

    add celery task

---
+++ blogapp/tasks.py
@@ -0,0 +1,7 @@
+from celery import shared_task
+from django.core.management import call_command
+
+
+@shared_task
+def perform_regular_task():
+    call_command('dumpdata', '--output=data_backup.json')
